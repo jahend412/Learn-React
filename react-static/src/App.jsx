6a5aeaed3d3be0be01ae64c880b2d88a04ff35fe
Josh Endemann <jahend44@gmail.com>
2023-07-23 11:24:30 -0500

    Switched to Vite
    Added Content to NavBar
    Added images folder in public
    added react-icon.png to images folder
    styled the navbar component in index.css

--- react-static/src/App.jsx
+++ react-static/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import Navbar from "/src/components/Navbar"
+import Main from "/src/components/Main"
 
 export default function App() {
   return (
