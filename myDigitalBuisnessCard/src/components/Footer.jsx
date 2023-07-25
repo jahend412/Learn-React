951a8c7b387459a162c66a3af38498a632c8109f
Josh Endemann <jahend44@gmail.com>
2023-07-24 19:30:47 -0500

    Styled Info Component

--- myDigitalBuisnessCard/src/components/Footer.jsx
+++ myDigitalBuisnessCard/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Footer() {
-    <div>
-        <h1>Hey</h1>
-        <a href='https://github.com/jahend412'>Github </a>
-        <a href='https://www.linkedin.com/in/joshua-endemann/'>LinkedIN</a>
-        <a href='https://twitter.com/joshendemann'>Twitter</a>
-    </div>
+    return (
+        <div className='main--footer'>
+            <a href='https://github.com/jahend412'><img src="/img/githublogo.png" alt="" /> </a>
+            <a href='https://www.linkedin.com/in/joshua-endemann/'><img src="/img/linkedinlogo.png" alt="" /></a>
+            <a href='https://twitter.com/joshendemann'><img src="/img/twitterlogo.png" alt="" /></a>
+        </div>
+    )
 }
