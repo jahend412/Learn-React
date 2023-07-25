7d70c2c7d6b841dfaebdfc2a7a87333a2fcfe67c
Josh Endemann <jahend44@gmail.com>
2023-07-25 12:32:42 -0500

    Setup AirBnb Clone site using Vite + React

---
+++ AirBnb-Clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '/src/components/NavBar'
+import './App.css'
+
+export default function App() {
+  return (
+
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+
