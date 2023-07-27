b2e290ee4f3efbb9d798b849b33d986bce3541ed
Josh Endemann <jahend44@gmail.com>
2023-07-26 19:40:58 -0500

    Mapped data over Card component

--- AirBnb-Clone/src/App.jsx
+++ AirBnb-Clone/src/App.jsx
@@ -2,26 +2,31 @@ import React, { useCallback } from 'react'
 import NavBar from '/src/components/NavBar'
 import Hero from '/src/components/Hero'
 import Card from '/src/components/Card'
+import data from '/src/data'
 
 
 
 import './App.css'
 
-export default function App() {
-  return (
+export default function App(props) {
+  // Hero
+  const cards = data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
 
+  return (
     <div>
       <NavBar />
-      <Hero />
-      <Card
-        img='card-photo.png'
-        rating='5.0'
-        reviewCount={6}
-        country='USA'
-        title='Life Lessons with Katie Zaferes'
-        price={136}
-      />
-
+      {cards}
     </div>
   )
 }
