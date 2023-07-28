85b4b6f7314e978ee2438f462922e07387e005ea
Josh Endemann <jahend44@gmail.com>
2023-07-28 12:47:40 -0500

    FInished my Concert project

--- my-concerts/src/App.jsx
+++ my-concerts/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
 
 
   return (
-    <div>
+    <div className="container">
       <NavBar />
       <section className="cards-list">
         {cards}
--- my-concerts/src/components/Card.jsx
+++ my-concerts/src/components/Card.jsx
@@ -4,17 +4,17 @@ export default function Card(props) {
 
     return (
         <div className="card">
-            <img src={props.item.coverImg} alt="" />
+            <img className="card--image" src={props.item.coverImg} alt="" />
             <div className="card--main">
                 <h2 className="card--band">{props.item.band}</h2>
                 <p className="card--description">{props.item.description}</p>
             </div>
 
-            <div className="card-area">
+            <div className="card--area">
                 <h4 className="card--date">{props.item.date}</h4>
-                <p className="card--tour">{props.item.tour}</p>
                 <h4 className="card--venue">{props.item.venue}</h4>
                 <p className="card--location">{props.item.location}</p>
+                <p className="card--tour">{props.item.tour}</p>
             </div>
 
 
