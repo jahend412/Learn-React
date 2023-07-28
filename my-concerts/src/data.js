81c7071f3b652bd3ae5c3af56b4cb2f8cf44077a
Josh Endemann <jahend44@gmail.com>
2023-07-27 19:13:45 -0500

    Create data.js

---
+++ my-concerts/src/data.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        id: 1,
+        Band: "Rush",
+        description: 'Rush was a Canadian rock band that primarily comprised Geddy Lee (bass, keyboards, vocals), Alex Lifeson (guitar), and Neil Peart (drums, percussion, lyricist). ',
+        price: 70,
+        coverImg: "rush.png",
+        date: '9/6/2007',
+        venue: "Marcus Amphitheater",
+        tour: 'Snakes & Arrows'
+
+    },
+    {
+        id: 2,
+        band: "Flaming Lips",
+        description: 'Colorful lyrics, otherworldly synths, and wild arrangements make up the psychedelic alt-rock delirium that is The Flaming Lips. For years, the band—presently consisting of Wayne Coyne, Steven Drozd, Derek Brown, Matt Kirksey, and Nicholas Ley—transfixed underground audiences with their weird and wonderful style.',
+        price: 15,
+        coverImg: "flaming-lips.png",
+        date: '7/8/2015',
+        venue: "SummerFest grounds",
+        tour: 'Summerfest'
+
+    },
+    {
+        id: 3,
+        band: "Phish",
+        description: 'Phish is an American rock band formed in Burlington, Vermont, in 1983. The band is known for musical improvisation, extended jams, blending of genres, and a dedicated fan base. The band consists of guitarist Trey Anastasio, bassist Mike Gordon, drummer Jon Fishman, and keyboardist Page McConnell, all of whom perform vocals, with Anastasio being the lead vocalist.',
+        price: 70,
+        coverImg: "phish.png",
+        date: '8/12/2022',
+        venue: "Alpine Valley",
+        tour: '2022 Summer Tour'
+    },
+    {
+        id: 4,
+        band: "Phish",
+        description: 'Phish is an American rock band formed in Burlington, Vermont, in 1983. The band is known for musical improvisation, extended jams, blending of genres, and a dedicated fan base. The band consists of guitarist Trey Anastasio, bassist Mike Gordon, drummer Jon Fishman, and keyboardist Page McConnell, all of whom perform vocals, with Anastasio being the lead vocalist.',
+        price: 70,
+        coverImg: "phish.png",
+        date: '8/13/2022',
+        venue: "Alpine Valley",
+        tour: '2022 Summer Tour'
+    },
+    {
+        id: 5,
+        band: "Phish",
+        description: 'Phish is an American rock band formed in Burlington, Vermont, in 1983. The band is known for musical improvisation, extended jams, blending of genres, and a dedicated fan base. The band consists of guitarist Trey Anastasio, bassist Mike Gordon, drummer Jon Fishman, and keyboardist Page McConnell, all of whom perform vocals, with Anastasio being the lead vocalist.',
+        price: 70,
+        coverImg: "phish.png",
+        date: '8/13/2022',
+        venue: "Alpine Valley",
+        tour: '2022 Summer Tour'
+    }
+]
