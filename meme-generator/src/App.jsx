daeb572aab8b3107da2fc08f2f9c855c38b52268
Josh Endemann <jahend44@gmail.com>
2023-07-29 12:45:44 -0500

    Got Random Images to display
    Used React useState to change the  state of the image.

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Header from '/src/components/Header'
 import Meme from "./components/Meme"
 
+
 export default function App() {
   return (
     <div className="container">
