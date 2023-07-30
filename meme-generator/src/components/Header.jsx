b3f5bcc3df865a9d95b87837f1d58bf5bed19163
Josh Endemann <jahend44@gmail.com>
2023-07-30 08:37:57 -0500

    Fixed Bug in styling

--- meme-generator/src/components/Header.jsx
+++ meme-generator/src/components/Header.jsx
@@ -4,7 +4,7 @@ import HeaderImg from '/troll-face.png'
 export default function Header() {
     return (
         <header className="header">
-            <img className="header--img" src={HeaderImg} />
+            <img className="header--image" src={HeaderImg} />
             <h2 className="header--title">Meme-Generator</h2>
             <h4 className="header-project">React Course - Project 3</h4>
         </header>
